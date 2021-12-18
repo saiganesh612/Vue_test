4a2ca69c3f01cd578014d278fe9708d04fc1b28c
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-12-18 19:59:05 +0530

    error with CORS

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,9 +1,58 @@
 <template>
-  <h2>This is Detailed Page</h2>
+  <div>
+    <form id="form" @submit.prevent="submitData">
+      <div>
+        <label for="age">Age: </label>
+        <input
+          type="number"
+          placeholder="Enter your age"
+          v-model.number="formValues.Age"
+        />
+      </div>
+      <div>
+        <label for="gender">Gender: </label>
+        <select id="gender" v-model="formValues.Gender">
+          <option value="">Select you Gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </div>
+      <div>
+        <label for="residence">Residence: </label>
+        <input
+          type="text"
+          id="residence"
+          list="states"
+          v-model="formValues.Residence"
+        />
+
+        <datalist id="states">
+          <option value="Telangana" />
+          <option value="Andhra Pradesh" />
+          <option value="Karnataka" />
+          <option value="Tamil Nadu" />
+          <option value="Kerala" />
+          <option value="Maharastra" />
+          <option value="Madhya Pradesh" />
+          <option value="Rajasthan" />
+          <option value="Jammu and Kashmir" />
+          <option value="Delhi" />
+          <option value="Gujarat" />
+        </datalist>
+      </div>
+
+      <div>
+        <button>Submit</button>
+      </div>
+    </form>
+
+    <div v-if="errMssg">{{ errMssg }}</div>
+  </div>
 </template>
 
 <script>
 import { handleAuth, isLoggedIn } from "../utils/auth";
+import axios from "axios";
 
 export default {
   name: "Details",
@@ -11,5 +60,55 @@ export default {
     handleAuth();
     if (!isLoggedIn()) this.$router.push("/");
   },
+  data() {
+    return {
+      formValues: {
+        Age: null,
+        Gender: "",
+        Residence: "",
+      },
+      errMssg: "",
+    };
+  },
+  methods: {
+    submitData() {
+      if (this.formValues.Age < 18) {
+        this.errMssg =
+          "Sorry - only candidates above 18 years of age are allowed";
+
+        window.location.reload();
+      } else {
+        const scriptUrl =
+          "https://script.google.com/macros/s/AKfycbxouWKEphWzZWD5bqgss1bZ2-W2ZsVLA29A5JmDjZL5u13x8KQPgq7Tv1RKMaI4F2Ca/exec";
+
+        const proxy = "https://cors-anywhere.herokuapp.com/";
+        axios({
+          method: "post",
+          url: proxy + scriptUrl,
+          data: JSON.stringify(this.formValues),
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+          mode: "no-cors",
+        })
+          .then((res) => console.log(res))
+          .catch((err) => console.log(err));
+
+        console.log(this.formValues);
+      }
+    },
+  },
 };
 </script>
+
+<style scoped>
+/* #form {
+  display: flex;
+  justify-content: center;
+} */
+
+#form > div {
+  width: fit-content;
+}
+</style>
